9373cfe68954c85bd474a0afcbada53782de9801
Paul Vencill <paul@cyberstride.net>
2014-08-01 17:13:19 -0400

    got it working

--- index.js
+++ index.js
@@ -18,9 +18,10 @@ app.use(cookieParser(config.secret));
 
 app.use(session({
   secret: config.secret,
+  cookieName: 'session',
   duration: 30*60*1000, //  thirty minutes
   cookie: {
-    ephemeral: true,
+    ephemeral: true
     //secureProxy:true // maybe for production?
   }
 }));
@@ -28,27 +29,11 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.post('/signin', function(req,res,next){
-  console.log('posting to signin');
-  passport.authenticate('ldapauth', function(err,user,info){
-    console.log('callback from ldapauth');
-    if(err){
-      console.log(err);
-      return next(err);
-    }
-    if(!user){
-      console.log(info);
-      return res.redirect('/login.html?message=Login Failed');
-    }
-    console.log(user);
-    return res.redirect('/');
-  })(req,res,next);
-});
 
-// app.post('/signin', passport.authenticate('ldapauth', {
-//   successRedirect: '/',
-//   failureRedirect:'/login.html'
-// }));
+app.post('/signin', passport.authenticate('ldapauth', {
+  successRedirect: '/',
+  failureRedirect:'/login.html'
+}));
 
 app.use(express.static(__dirname+'/public'));
 app.use(requiresLogin);
